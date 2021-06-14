b939c39a2d16cf7082396b231593107bce73c7bd
LeoDELPON <leo.delponviacesi@gmail.com>
2021-06-14 18:38:37 +0200

    Fix responsive for 1215 / 1082

--- components/projects-and-partners.js
+++ components/projects-and-partners.js
@@ -6,14 +6,14 @@ export default function ProjectsAndPartners()
 
 	return (
 		<section className="my-12 md:my-0 w-full h-37_6 md:h-full bg-white relative z-20 grid grid-rows-3 grid-cols-3 xl:grid-rows-none xl:flex xl:justify-around justify-items-center" >
-			<img src="/images/icons/coinflex-icon.svg" className="w-partnerImg h-partnerImgMobile md:h-partnerImg md:my-8"/>
-			<img src="/images/icons/spice-icon.svg" className="w-partnerImg h-partnerImgMobile md:h-partnerImg md:my-8"/>
-			<img src="/images/icons/sideshift-icon.svg" className="w-partnerImg h-partnerImgMobile md:h-partnerImg md:my-8"/>
-			<img src="/images/icons/coinex-icon.svg" className="w-partnerImg h-partnerImgMobile md:h-partnerImg md:my-8"/>
-			<img src="/images/icons/IFwallet-icon.svg" className="w-partnerImg h-partnerImgMobile md:h-partnerImg md:my-8"/>
-			<img src="/images/icons/zapit-icon.svg" className="w-partnerImg h-partnerImgMobile md:h-partnerImg md:my-8"/>
-			<img src="/images/icons/badger-icon.svg" className="w-partnerImg h-partnerImgMobile md:h-partnerImg md:my-8"/>
-			<img src="/images/icons/electron-icon.svg" className="w-partnerImg h-partnerImgMobile md:h-partnerImg md:my-8"/>
+			<img src="/images/icons/coinflex-icon.svg" className="w-partnerImg h-partnerImgMobile md:h-partnerImg lg:my-5"/>
+			<img src="/images/icons/spice-icon.svg" className="w-partnerImg h-partnerImgMobile md:h-partnerImg lg:my-5"/>
+			<img src="/images/icons/sideshift-icon.svg" className="w-partnerImg h-partnerImgMobile md:h-partnerImg lg:my-5"/>
+			<img src="/images/icons/coinex-icon.svg" className="w-partnerImg h-partnerImgMobile md:h-partnerImg lg:my-5"/>
+			<img src="/images/icons/IFwallet-icon.svg" className="w-partnerImg h-partnerImgMobile md:h-partnerImg lg:my-5"/>
+			<img src="/images/icons/zapit-icon.svg" className="w-partnerImg h-partnerImgMobile md:h-partnerImg lg:my-5"/>
+			<img src="/images/icons/badger-icon.svg" className="w-partnerImg h-partnerImgMobile md:h-partnerImg lg:my-5"/>
+			<img src="/images/icons/electron-icon.svg" className="w-partnerImg h-partnerImgMobile md:h-partnerImg lg:my-5"/>
 		</section>
 	);
 } 
