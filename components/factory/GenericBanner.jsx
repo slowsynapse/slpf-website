2d251edccc25c4264a96dc2634de29c18e3bc814
LeoDELPON <leo.delponviacesi@gmail.com>
2021-06-14 12:59:50 +0200

    Add padding to banner container

--- components/factory/GenericBanner.jsx
+++ components/factory/GenericBanner.jsx
@@ -13,7 +13,7 @@ const GenericBanner = ({
       <div className="bg-gradient-to-b from-groupBanner-primary via-groupBanner-primary to-groupBanner-secondary">
         <div className="w-full bg-gradient-to-l from-hero-primary via-hero-secondary to-transparent relative z-0">
           <div
-            className={`flex flex-col justify-around container sm:max-w-screen-xsm md:max-w-screen-md xl:max-w-screen-1440p mx-auto max-h-64 md:max-h-full xsm:py-4 md:py-12 ${bannerRule}`}
+            className={`flex flex-col justify-around container sm:max-w-screen-xsm md:max-w-screen-md xl:max-w-screen-1440p mx-auto max-h-64 md:max-h-full xsm:py-4 md:py-12 md:px-5 ${bannerRule}`}
           >
             <h1 className={`font-bold text-white md:text-4xl text-xl md:w-144 font-gilroyLight ${titleClasses}`}>
               {title}
